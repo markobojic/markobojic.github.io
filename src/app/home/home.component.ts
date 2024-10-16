98de19669bf1b23a7f2b1745892a543776750f5c
markobojic <bojic.marko021@gmail.com>
2024-10-16 11:20:01 +0200

    testimonials component with header

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { AboutComponent } from './components/about/about.component';
 import { CompanyListComponent } from './components/company-list/company-list.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AboutComponent, CompanyListComponent],
+  imports: [AboutComponent, CompanyListComponent, TestimonialsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
