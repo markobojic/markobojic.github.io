ceccc0e2dbc422c054463dbdb81d3b994d8ac914
markobojic <bojic.marko021@gmail.com>
2024-10-27 08:39:07 +0100

    testimonials data

--- src/app/home/components/testimonials/testimonials.data.ts
+++ src/app/home/components/testimonials/testimonials.data.ts
@@ -3,6 +3,13 @@ import { Testimonial } from './testimonial.model';
 // TODO: Temp solution for MVP
 export const TESTIMONIALS_DATA: Testimonial[] = [
   {
+    name: 'Ronald van den Berg',
+    feedback:
+      'Marko is a creative and dedicated front-end developer, that has the ability to bring designs to life. Best of all, he is able to do so without losing track of customer needs. Marko knows how to manage expectations and he communicates clearly. His work has always been excellent and it was a pleasure to work with him.',
+    position: 'Technical Consultant at Enigmatry',
+    imageSrc: 'assets/testimonials/ronald.jpeg',
+  },
+  {
     name: 'Shane Armstrong',
     feedback:
       'Marko effectively led his team through a complex project, providing objective feedback and pragmatic solutions, while navigating a difficult environment with legacy technology and significant challenges.',
@@ -10,15 +17,24 @@ export const TESTIMONIALS_DATA: Testimonial[] = [
     imageSrc: 'assets/testimonials/shane.jpeg',
   },
   {
-    name: 'Jane Smith',
-    feedback: 'I absolutely loved the experience!',
-    position: 'CTO, Tech Solutions',
-    imageSrc: 'assets/jane-smith.jpg',
+    name: 'Jelle de Haan',
+    feedback:
+      'It was a pleasure working with Marko for several years. Marko is a good communicator and front-end developer.',
+    position: 'Partner & CEO, Enigmatry',
+    imageSrc: 'assets/testimonials/jelle.jpeg',
   },
   {
-    name: 'Alex Johnson',
-    feedback: 'Highly recommend to anyone looking for quality!',
-    position: 'Product Manager, Web Corp',
-    imageSrc: 'assets/alex-johnson.jpg',
+    name: 'Simon Elgar',
+    feedback:
+      'Marko is a talented, versatile and proactive developer whom I thoroughly enjoyed working with. He is intuitive, easy to work with and had a really positive impact on the team.',
+    position: 'Director, Monochrome',
+    imageSrc: 'assets/testimonials/simon.jpeg',
+  },
+  {
+    name: 'Scott Davies',
+    feedback:
+      'It was great working with Marko at Monochrome. He was a valuable asset to the team and made a strong impact during his time with us.',
+    position: 'Head of Development, Monochrome',
+    imageSrc: 'assets/testimonials/scott.jpeg',
   },
 ];
