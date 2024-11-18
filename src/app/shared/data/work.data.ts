e8ba5543281e1c8bbed9b342b47206c3c1595baa
markobojic <bojic.marko021@gmail.com>
2024-11-18 16:20:12 +0100

    work summary data

--- src/app/shared/data/work.data.ts
+++ src/app/shared/data/work.data.ts
@@ -100,7 +100,7 @@ Pored toga vodis razvoj projekata na koje si dodeljen`,
     position: 'Front-end Lead / Partner',
     company: 'Rockdata',
     dateStart: 'Mar 2020',
-    dateEnd: 'Jun 2022',
+    dateEnd: 'May 2022',
     responsibilities: `Ovo je opis u kom treba da napises da si reporting menadzer 8 developera. Da kreiras godisnje ciljeve i radis aprejzale sa njima, pratis njihov razvoj, vodis racuna o godisnjim odmorima itd.
 Pored toga vodis razvoj projekata na koje si dodeljen`,
     skills: ['Team leadership', 'Engineering Management'],
@@ -189,6 +189,29 @@ Pored toga vodis razvoj projekata na koje si dodeljen`,
   },
   {
     id: '9',
+    position: 'Front-End Developer / Co-founder',
+    company: 'Awerest',
+    dateStart: 'Feb 2014',
+    dateEnd: 'Dec 2015',
+    responsibilities: `Ovo je opis u kom treba da napises da si reporting menadzer 8 developera. Da kreiras godisnje ciljeve i radis aprejzale sa njima, pratis njihov razvoj, vodis racuna o godisnjim odmorima itd.
+Pored toga vodis razvoj projekata na koje si dodeljen`,
+    skills: ['Team leadership', 'Engineering Management'],
+    projects: [
+      {
+        title: 'Project 1',
+        description: `Ovo je opis u kom treba da napises da si reporting menadzer 8 developera. Da kreiras godisnje ciljeve i radis aprejzale sa njima, pratis njihov razvoj, vodis racuna o godisnjim odmorima itd.
+Pored toga vodis razvoj projekata na koje si dodeljen`,
+        technologies: ['angular', 'typescript'],
+      },
+      {
+        title: 'Project 2',
+        description: `I lead a team of over 10 engineers and QA specialists in migrating a complex stock market solution to Angular codebase with a REST API. My role involves managing the project, providing technical direction, and supporting team development. I worked with delivery directors on reporting and coordinate with client project managers on planning and delivery, addressing both technical and domain-specific challenges.`,
+        technologies: ['angular', 'typescript'],
+      },
+    ],
+  },
+  {
+    id: '10',
     position: 'Level Designer',
     company: 'Eipix',
     dateStart: 'May 2014',
