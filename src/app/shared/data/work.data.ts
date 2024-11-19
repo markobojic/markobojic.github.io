1407a273ed1c3cf27f4e5a5d429a131c656ef956
markobojic <bojic.marko021@gmail.com>
2024-11-19 15:24:34 +0100

    syne work data

--- src/app/shared/data/work.data.ts
+++ src/app/shared/data/work.data.ts
@@ -9,19 +9,17 @@ export const WORK_DATA: Job[] = [
     company: 'Synechron',
     dateStart: 'Jan 2024',
     dateEnd: 'Present',
-    responsibilities: `Ovo je opis u kom treba da napises da si reporting menadzer 8 developera. Da kreiras godisnje ciljeve i radis aprejzale sa njima, pratis njihov razvoj, vodis racuna o godisnjim odmorima itd.
-Pored toga vodis razvoj projekata na koje si dodeljen`,
+    responsibilities: `As a Tech Lead, I combine hands-on development with leadership by collaborating closely with clients, ensuring delivery timelines, and addressing risks. I mentor team members, oversee task delegation, and manage operational duties like resource allocation and recruitment.`,
     skills: ['Team leadership', 'Engineering Management'],
     projects: [
       {
         title: 'Project 1',
-        description: `Ovo je opis u kom treba da napises da si reporting menadzer 8 developera. Da kreiras godisnje ciljeve i radis aprejzale sa njima, pratis njihov razvoj, vodis racuna o godisnjim odmorima itd.
-Pored toga vodis razvoj projekata na koje si dodeljen`,
-        technologies: ['angular', 'typescript'],
+        description: `I lead the creation of a reusable UI library using Storybook, building modular microfrontends for multiple applications, integrated with GraphQL. My role includes sprint-based development and ensuring deliverables meet client expectations while addressing potential risks.`,
+        technologies: ['angular', 'typescript', 'tailwind', 'graphql'],
       },
       {
         title: 'Project 2',
-        description: `I lead a team of over 10 engineers and QA specialists in migrating a complex stock market solution to Angular codebase with a REST API. My role involves managing the project, providing technical direction, and supporting team development. I worked with delivery directors on reporting and coordinate with client project managers on planning and delivery, addressing both technical and domain-specific challenges.`,
+        description: `I led a team to migrate a Silverlight-based solution to an Angular framework with REST APIs, tackling challenges like missing requirements and legacy code analysis. My role involved defining functionality from poorly written code, providing technical guidance, and aligning with clients on delivery goals.`,
         technologies: ['angular', 'typescript'],
       },
     ],
