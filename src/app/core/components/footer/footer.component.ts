6614c1a86149000f8e4255b0d03f772fcf33cf32
markobojic <bojic.marko021@gmail.com>
2025-01-23 15:19:16 +0100

    layout improvements

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavLinkComponent } from '../nav-link/nav-link.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [NavLinkComponent],
+  imports: [NavLinkComponent, RouterModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
