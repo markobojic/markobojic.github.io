1a89ee329fda4d21bc58aa436945035625adbe98
markobojic <bojic.marko021@gmail.com>
2025-01-23 15:28:57 +0100

    remove lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,27 +1,21 @@
 import { Routes } from '@angular/router';
 import { WorkComponent } from './pages/work/work.component';
 import { WorkDetailsComponent } from './pages/work-details/work-details.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () =>
-      import('./pages/home/home.component').then((c) => c.HomeComponent),
+    component: HomeComponent,
   },
   {
     path: 'work',
     component: WorkComponent,
-    // loadComponent: () =>
-    //   import('./pages/work/work.component').then((c) => c.WorkComponent),
     data: { animation: 'Page1' },
   },
   {
     path: 'work/:id',
     component: WorkDetailsComponent,
-    // loadComponent: () =>
-    //   import('./pages/work-details/work-details.component').then(
-    //     (c) => c.WorkDetailsComponent
-    //   ),
     data: { animation: 'Page2' },
   },
   {
