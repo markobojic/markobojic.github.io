3cf0617c462215dc7d25ec8423b1a5e014671ef3
markobojic <bojic.marko021@gmail.com>
2025-01-23 15:40:33 +0100

    animation glich

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -10,30 +10,46 @@ import {
 export const swipeAnimation = trigger('routeAnimations', [
   transition('Page1 => Page2', [
     style({ position: 'relative' }),
-    query(':enter, :leave', [
-      style({
-        position: 'absolute',
-        width: '100%',
-      }),
-    ]),
-    query(':enter', [style({ left: '100%' })]),
+    query(
+      ':enter, :leave',
+      [
+        style({
+          position: 'absolute',
+          width: '100%',
+        }),
+      ],
+      { optional: true }
+    ),
+    query(':enter', [style({ left: '100%' })], { optional: true }),
     group([
-      query(':leave', [animate('300ms ease-out', style({ left: '-100%' }))]),
-      query(':enter', [animate('300ms ease-out', style({ left: '0%' }))]),
+      query(':leave', [animate('300ms ease-out', style({ left: '-100%' }))], {
+        optional: true,
+      }),
+      query(':enter', [animate('300ms ease-out', style({ left: '0%' }))], {
+        optional: true,
+      }),
     ]),
   ]),
   transition('Page2 => Page1', [
     style({ position: 'relative' }),
-    query(':enter, :leave', [
-      style({
-        position: 'absolute',
-        width: '100%',
-      }),
-    ]),
-    query(':enter', [style({ left: '-100%' })]),
+    query(
+      ':enter, :leave',
+      [
+        style({
+          position: 'absolute',
+          width: '100%',
+        }),
+      ],
+      { optional: true }
+    ),
+    query(':enter', [style({ left: '-100%' })], { optional: true }),
     group([
-      query(':leave', [animate('300ms ease-out', style({ left: '100%' }))]),
-      query(':enter', [animate('300ms ease-out', style({ left: '0%' }))]),
+      query(':leave', [animate('300ms ease-out', style({ left: '100%' }))], {
+        optional: true,
+      }),
+      query(':enter', [animate('300ms ease-out', style({ left: '0%' }))], {
+        optional: true,
+      }),
     ]),
   ]),
 ]);
